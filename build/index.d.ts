71d1b3fdcecfef18a3d35a71514c595282099ca7
JustANutshell <darklolcrap@gmail.com>
2020-05-06 19:32:21 +0200

    stuff

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,4 +2,7 @@ import { arrFind, arrFindFuncAsync, arrFindAsync } from "./arrFind";
 import { arrFilter, arrFilterFuncAsync, arrFilterAsync } from "./arrFilter";
 import { arrConnects } from "./arrConnects";
 import { arrRmEntry } from "./arrRmEntry";
-export { arrFind, arrFindFuncAsync, arrFindAsync, arrFilter, arrFilterFuncAsync, arrFilterAsync, arrConnects, arrRmEntry };
+export { arrFind, arrFindFuncAsync, arrFindAsync, };
+export { arrFilter, arrFilterFuncAsync, arrFilterAsync, };
+export { arrConnects, };
+export { arrRmEntry, };
