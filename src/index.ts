fe9aea1bdc3c396a6244582a865a8aef70b38357
JustANutshell <darklolcrap@gmail.com>
2020-05-08 16:53:11 +0200

    moved files

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 // array
 
-import {arrFind,arrFindFuncAsync,arrFindAsync} from "./arrFind";
+import {arrFind,arrFindFuncAsync,arrFindAsync} from "./arr/arrFind";
 export {arrFind,arrFindFuncAsync,arrFindAsync,}
 
-import {arrFilter,arrFilterFuncAsync,arrFilterAsync} from "./arrFilter";
+import {arrFilter,arrFilterFuncAsync,arrFilterAsync} from "./arr/arrFilter";
 export {arrFilter,arrFilterFuncAsync,arrFilterAsync,};
 
-import {arrConnects} from "./arrConnects"
+import {arrConnects} from "./arr/arrConnects"
 export {arrConnects,};
 
-import {arrRmEntry} from "./arrRmEntry"
+import {arrRmEntry} from "./arr/arrRmEntry"
 export {arrRmEntry,};
 
-import {arrRmDoubles} from "./arrRmDoubles"
+import {arrRmDoubles} from "./arr/arrRmDoubles"
 export {arrRmDoubles,};
 
-import {arrEntry,arrEntryGetFirstEntry,arrEntryGetFirstLastMissEntry,arrEntryGetFirstMissEntry,arrEntryGetLastEntry,arrEntryGetLastFirstMissEntry,arrEntryGetLastMissEntry} from "./arrEntry"
+import {arrEntry,arrEntryGetFirstEntry,arrEntryGetFirstLastMissEntry,arrEntryGetFirstMissEntry,arrEntryGetLastEntry,arrEntryGetLastFirstMissEntry,arrEntryGetLastMissEntry} from "./arr/arrEntry"
 export {arrEntry,arrEntryGetFirstEntry,arrEntryGetFirstLastMissEntry,arrEntryGetFirstMissEntry,arrEntryGetLastEntry,arrEntryGetLastFirstMissEntry,arrEntryGetLastMissEntry,};
 
 // string
 
-import {strFill} from "./strFill"
+import {strFill} from "./str/strFill"
 export {strFill,};
+
